15af83e42ea8105ca47e5a062aa1a801db04af0b
shadialahmd <shadi.alahmd@gmail.com>
2021-08-22 11:02:44 +0400

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,41 @@
 <?php 
-define('DB_HOST', 'localhost'); 
-define('DB_USER','root'); 
-define('DB_NAME', 'bookstore'); 
-define('DB_PASSWORD',''); 
+// define('DB_HOST', 'localhost'); 
+// define('DB_USER','root'); 
+// define('DB_NAME', 'bookstore'); 
+// define('DB_PASSWORD',''); 
 
-$con=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); 
-$db=mysqli_select_db($con,DB_NAME) or die("Failed to connect to MySQL: " . mysql_error()); 
+// $con=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); 
+// $db=mysqli_select_db($con,DB_NAME) or die("Failed to connect to MySQL: " . mysql_error()); 
+
+
+
+
+
+class Database{
+
+
+    private $host='localhos';
+    private $user='root';
+    private $pass='';
+    private $dbname='bookstore';
+
+    public $conn=null;
+
+
+    public function getconnection(){
+
+
+        $conn=mysqli_connect($this->host,$this->user,$this->pass,$this->dbname);
+
+        if(!$conn){
+
+            die("Error in DB connection".mysqli_error());
+        }
+        return $conn;
+    }
+
+}
+
+
+
 ?>
